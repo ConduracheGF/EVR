20c5dd971a0e5b9000b1e96459b072b3b2f8d2b2
ConduracheGF <condurachegabriel14@gmail.com>
2025-12-27 19:11:11 +0200

    Actualizare: afisare numere negative

--- Arduino/7segment_I2C/i2c_main/i2c_main.ino
+++ Arduino/7segment_I2C/i2c_main/i2c_main.ino
@@ -9,6 +9,7 @@ int main() {
 
     int cifra_input[5] = {0};
     int cnt=0, dp=-1;
+    int negativ = 0;
 
     while (1) {
         unsigned char in = USART_Receive();
@@ -24,24 +25,37 @@ int main() {
                 dp = cnt - 1;
             }
         }
+        else if (in == '-' && cnt == 0) {
+            negativ = 1;
+        }
         else if (in == ' ' || in == '\n') {
             int cifra[4] = {0};
-            for (int i = 0; i < 4; i++) {
-                if (i < cnt)
-                    cifra[i] = cifra_input[i];
-                else
-                    cifra[i] = 0;
+            if(negativ){
+                for (int i = 3; i > 0; i--) {
+                    if (i-1 < cnt)
+                        cifra[i] = cifra_input[i-1];
+                    else
+                        cifra[i] = 0;
+                }
+                cifra[0] = 0x0A;
+            } else {
+                for (int i = 0; i < 4; i++) {
+                    if (i < cnt)
+                        cifra[i] = cifra_input[i];
+                    else
+                        cifra[i] = 0;
+                }
             }
-            
             if (dp >= 0 && dp < 4) {
-                cifra[dp] |= 0x80;
-            } else if (cnt < 4) {
-                cifra[cnt-1] |= 0x80;
+                cifra[negativ ? dp+1 : dp] |= 0x80;
+            } else if (cnt > 0 && cnt < 4) {
+                cifra[negativ ? cnt : cnt-1] |= 0x80;
             }
             
             afisare(cifra);
             cnt = 0;
             dp = -1;
+            negativ = 0;
         }
     }
 }
