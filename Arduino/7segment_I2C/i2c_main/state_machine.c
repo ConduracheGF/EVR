9dd496846c2ecce4f12d6c7bdde6e1c6e06047ae
ConduracheGF <condurachegabriel14@gmail.com>
2026-01-05 15:15:05 +0200

    Final?

--- Arduino/7segment_I2C/i2c_main/state_machine.c
+++ Arduino/7segment_I2C/i2c_main/state_machine.c
@@ -5,14 +5,20 @@ i2c_bus_state_t i2c_bus_state = I2C_OK;
 driver_state_t driver_state = DRIVER_NEEDS_INIT;
 
 void driver_task(uint16_t tensiune){
+  static uint16_t counter = 0;
   switch(driver_state){
     case DRIVER_NEEDS_INIT:
+      counter = 0;
       if(setup_7_segm() == SEND_SUCCESSFUL){
         driver_state = DRIVER_OK;
       }
       break;
     case DRIVER_OK:
       if(afisare_tensiune(tensiune) != SEND_SUCCESSFUL){
+        driver_state = DRIVER_NEEDS_INIT;
+      }
+      counter++;
+      if(counter >= 50){
         driver_state = DRIVER_NEEDS_INIT;
       }
       break;
