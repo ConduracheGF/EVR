449078b91e01da9411df853be184a2b33aa54381
ConduracheGF <condurachegabriel14@gmail.com>
2026-02-24 13:57:53 +0200

    reglementarile de azi

---
+++ Arduino/I2C_Oficial/Dio_Example_S32K144/Debug_FLASH/src/7-segment-display.d
@@ -0,0 +1,132 @@
+src/7-segment-display.o: ../src/7-segment-display.c \
+ C\:/Users/EVR/workspaceS32DS.3.6.0/Dio_Example_S32K144/include/7-segment-display.h \
+ ../RTD/include/Mcu.h \
+ C\:/Users/EVR/workspaceS32DS.3.6.0/Dio_Example_S32K144/generate/include/Mcu_Cfg.h \
+ C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K1_S32M24/RTD/BaseNXP_TS_T40D2M20I0R0/include/Mcal.h \
+ C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K1_S32M24/RTD/BaseNXP_TS_T40D2M20I0R0/include/Compiler.h \
+ C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K1_S32M24/RTD/BaseNXP_TS_T40D2M20I0R0/include/Compiler_Cfg.h \
+ C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K1_S32M24/RTD/BaseNXP_TS_T40D2M20I0R0/include/CompilerDefinition.h \
+ C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K1_S32M24/RTD/BaseNXP_TS_T40D2M20I0R0/include/StandardTypes.h \
+ C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K1_S32M24/RTD/BaseNXP_TS_T40D2M20I0R0/include/Platform_Types.h \
+ C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K1_S32M24/RTD/BaseNXP_TS_T40D2M20I0R0/include/PlatformTypes.h \
+ C\:/Users/EVR/workspaceS32DS.3.6.0/Dio_Example_S32K144/generate/include/Soc_Ips.h \
+ C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K1_S32M24/RTD/BaseNXP_TS_T40D2M20I0R0/include/Platform_Types.h \
+ C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K1_S32M24/RTD/BaseNXP_TS_T40D2M20I0R0/include/IpVersionMacros.h \
+ C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K1_S32M24/RTD/BaseNXP_TS_T40D2M20I0R0/include/Reg_eSys.h \
+ C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K1_S32M24/RTD/BaseNXP_TS_T40D2M20I0R0/include/OsIf_Internal.h \
+ C\:/Users/EVR/workspaceS32DS.3.6.0/Dio_Example_S32K144/generate/include/OsIf_Cfg.h \
+ C\:/Users/EVR/workspaceS32DS.3.6.0/Dio_Example_S32K144/generate/include/OsIf_ArchCfg.h \
+ C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K1_S32M24/RTD/BaseNXP_TS_T40D2M20I0R0/include/StandardTypes.h \
+ C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K1_S32M24/RTD/BaseNXP_TS_T40D2M20I0R0/header/S32K144_SYSTICK.h \
+ C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K1_S32M24/RTD/BaseNXP_TS_T40D2M20I0R0/header/S32K144_COMMON.h \
+ C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K1_S32M24/RTD/BaseNXP_TS_T40D2M20I0R0/include/BasicTypes.h \
+ C\:/Users/EVR/workspaceS32DS.3.6.0/Dio_Example_S32K144/generate/include/Clock_Ip_Cfg.h \
+ C\:/Users/EVR/workspaceS32DS.3.6.0/Dio_Example_S32K144/generate/include/Clock_Ip_VS_0_PBcfg.h \
+ ../RTD/include/Clock_Ip_Types.h \
+ C\:/Users/EVR/workspaceS32DS.3.6.0/Dio_Example_S32K144/generate/include/Clock_Ip_Cfg_Defines.h \
+ C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K1_S32M24/RTD/BaseNXP_TS_T40D2M20I0R0/header/S32K144_SIM.h \
+ C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K1_S32M24/RTD/BaseNXP_TS_T40D2M20I0R0/header/S32K144_SCG.h \
+ C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K1_S32M24/RTD/BaseNXP_TS_T40D2M20I0R0/header/S32K144_PCC.h \
+ C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K1_S32M24/RTD/BaseNXP_TS_T40D2M20I0R0/header/S32K144_SMC.h \
+ C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K1_S32M24/RTD/BaseNXP_TS_T40D2M20I0R0/include/Mcu_MemMap.h \
+ C\:/Users/EVR/workspaceS32DS.3.6.0/Dio_Example_S32K144/generate/include/Power_Ip_Cfg.h \
+ C\:/Users/EVR/workspaceS32DS.3.6.0/Dio_Example_S32K144/generate/include/Power_Ip_VS_0_PBcfg.h \
+ ../RTD/include/Power_Ip_Types.h \
+ C\:/Users/EVR/workspaceS32DS.3.6.0/Dio_Example_S32K144/generate/include/Power_Ip_Cfg_Defines.h \
+ C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K1_S32M24/RTD/BaseNXP_TS_T40D2M20I0R0/header/S32K144_PMC.h \
+ C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K1_S32M24/RTD/BaseNXP_TS_T40D2M20I0R0/header/S32K144_RCM.h \
+ ../RTD/include/Power_Ip_SMC_Types.h ../RTD/include/Power_Ip_RCM_Types.h \
+ ../RTD/include/Power_Ip_PMC_Types.h \
+ C\:/Users/EVR/workspaceS32DS.3.6.0/Dio_Example_S32K144/generate/include/Ram_Ip_Cfg.h \
+ C\:/Users/EVR/workspaceS32DS.3.6.0/Dio_Example_S32K144/generate/include/Ram_Ip_VS_0_PBcfg.h \
+ ../RTD/include/Ram_Ip_Types.h \
+ C\:/Users/EVR/workspaceS32DS.3.6.0/Dio_Example_S32K144/generate/include/Ram_Ip_Cfg_Defines.h \
+ C\:/Users/EVR/workspaceS32DS.3.6.0/Dio_Example_S32K144/generate/include/Mcu_VS_0_PBcfg.h \
+ ../RTD/include/Mcu_EnvCfg.h ../RTD/include/Mcu_Ipw_Types.h \
+ ../RTD/include/Clock_Ip_Types.h ../RTD/include/Power_Ip_Types.h \
+ ../RTD/include/Ram_Ip_Types.h ../RTD/include/CDD_I2c.h \
+ ../RTD/include/I2c_Types.h ../RTD/include/I2c_Ipw_Types.h \
+ ../RTD/include/Lpi2c_Ip_Types.h \
+ C\:/Users/EVR/workspaceS32DS.3.6.0/Dio_Example_S32K144/generate/include/Lpi2c_Ip_CfgDefines.h \
+ C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K1_S32M24/RTD/BaseNXP_TS_T40D2M20I0R0/header/S32K144_LPI2C.h \
+ ../RTD/include/Lpi2c_Ip_Callbacks.h ../RTD/include/Lpi2c_Ip_Features.h \
+ C\:/Users/EVR/workspaceS32DS.3.6.0/Dio_Example_S32K144/generate/include/CDD_I2c_CfgDefines.h \
+ C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K1_S32M24/RTD/BaseNXP_TS_T40D2M20I0R0/include/I2c_MemMap.h \
+ C\:/Users/EVR/workspaceS32DS.3.6.0/Dio_Example_S32K144/generate/include/CDD_I2c_Cfg.h \
+ C\:/Users/EVR/workspaceS32DS.3.6.0/Dio_Example_S32K144/generate/include/CDD_I2c_VS_0_PBcfg.h \
+ ../RTD/include/I2c_Types.h ../RTD/include/Dem.h \
+ C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K1_S32M24/RTD/BaseNXP_TS_T40D2M20I0R0/include/Std_Types.h \
+ C\:/Users/EVR/workspaceS32DS.3.6.0/Dio_Example_S32K144/generate/include/Dem_IntErrId.h \
+ ../RTD/include/Dem_Types.h ../RTD/include/Rte_Dem_Type.h \
+ ../RTD/include/Dem_Types.h ../RTD/include/Rte_Dem_Type.h \
+ C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K1_S32M24/RTD/BaseNXP_TS_T40D2M20I0R0/include/Dem_MemMap.h
+C\:/Users/EVR/workspaceS32DS.3.6.0/Dio_Example_S32K144/include/7-segment-display.h:
+../RTD/include/Mcu.h:
+C\:/Users/EVR/workspaceS32DS.3.6.0/Dio_Example_S32K144/generate/include/Mcu_Cfg.h:
+C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K1_S32M24/RTD/BaseNXP_TS_T40D2M20I0R0/include/Mcal.h:
+C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K1_S32M24/RTD/BaseNXP_TS_T40D2M20I0R0/include/Compiler.h:
+C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K1_S32M24/RTD/BaseNXP_TS_T40D2M20I0R0/include/Compiler_Cfg.h:
+C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K1_S32M24/RTD/BaseNXP_TS_T40D2M20I0R0/include/CompilerDefinition.h:
+C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K1_S32M24/RTD/BaseNXP_TS_T40D2M20I0R0/include/StandardTypes.h:
+C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K1_S32M24/RTD/BaseNXP_TS_T40D2M20I0R0/include/Platform_Types.h:
+C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K1_S32M24/RTD/BaseNXP_TS_T40D2M20I0R0/include/PlatformTypes.h:
+C\:/Users/EVR/workspaceS32DS.3.6.0/Dio_Example_S32K144/generate/include/Soc_Ips.h:
+C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K1_S32M24/RTD/BaseNXP_TS_T40D2M20I0R0/include/Platform_Types.h:
+C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K1_S32M24/RTD/BaseNXP_TS_T40D2M20I0R0/include/IpVersionMacros.h:
+C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K1_S32M24/RTD/BaseNXP_TS_T40D2M20I0R0/include/Reg_eSys.h:
+C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K1_S32M24/RTD/BaseNXP_TS_T40D2M20I0R0/include/OsIf_Internal.h:
+C\:/Users/EVR/workspaceS32DS.3.6.0/Dio_Example_S32K144/generate/include/OsIf_Cfg.h:
+C\:/Users/EVR/workspaceS32DS.3.6.0/Dio_Example_S32K144/generate/include/OsIf_ArchCfg.h:
+C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K1_S32M24/RTD/BaseNXP_TS_T40D2M20I0R0/include/StandardTypes.h:
+C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K1_S32M24/RTD/BaseNXP_TS_T40D2M20I0R0/header/S32K144_SYSTICK.h:
+C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K1_S32M24/RTD/BaseNXP_TS_T40D2M20I0R0/header/S32K144_COMMON.h:
+C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K1_S32M24/RTD/BaseNXP_TS_T40D2M20I0R0/include/BasicTypes.h:
+C\:/Users/EVR/workspaceS32DS.3.6.0/Dio_Example_S32K144/generate/include/Clock_Ip_Cfg.h:
+C\:/Users/EVR/workspaceS32DS.3.6.0/Dio_Example_S32K144/generate/include/Clock_Ip_VS_0_PBcfg.h:
+../RTD/include/Clock_Ip_Types.h:
+C\:/Users/EVR/workspaceS32DS.3.6.0/Dio_Example_S32K144/generate/include/Clock_Ip_Cfg_Defines.h:
+C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K1_S32M24/RTD/BaseNXP_TS_T40D2M20I0R0/header/S32K144_SIM.h:
+C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K1_S32M24/RTD/BaseNXP_TS_T40D2M20I0R0/header/S32K144_SCG.h:
+C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K1_S32M24/RTD/BaseNXP_TS_T40D2M20I0R0/header/S32K144_PCC.h:
+C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K1_S32M24/RTD/BaseNXP_TS_T40D2M20I0R0/header/S32K144_SMC.h:
+C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K1_S32M24/RTD/BaseNXP_TS_T40D2M20I0R0/include/Mcu_MemMap.h:
+C\:/Users/EVR/workspaceS32DS.3.6.0/Dio_Example_S32K144/generate/include/Power_Ip_Cfg.h:
+C\:/Users/EVR/workspaceS32DS.3.6.0/Dio_Example_S32K144/generate/include/Power_Ip_VS_0_PBcfg.h:
+../RTD/include/Power_Ip_Types.h:
+C\:/Users/EVR/workspaceS32DS.3.6.0/Dio_Example_S32K144/generate/include/Power_Ip_Cfg_Defines.h:
+C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K1_S32M24/RTD/BaseNXP_TS_T40D2M20I0R0/header/S32K144_PMC.h:
+C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K1_S32M24/RTD/BaseNXP_TS_T40D2M20I0R0/header/S32K144_RCM.h:
+../RTD/include/Power_Ip_SMC_Types.h:
+../RTD/include/Power_Ip_RCM_Types.h:
+../RTD/include/Power_Ip_PMC_Types.h:
+C\:/Users/EVR/workspaceS32DS.3.6.0/Dio_Example_S32K144/generate/include/Ram_Ip_Cfg.h:
+C\:/Users/EVR/workspaceS32DS.3.6.0/Dio_Example_S32K144/generate/include/Ram_Ip_VS_0_PBcfg.h:
+../RTD/include/Ram_Ip_Types.h:
+C\:/Users/EVR/workspaceS32DS.3.6.0/Dio_Example_S32K144/generate/include/Ram_Ip_Cfg_Defines.h:
+C\:/Users/EVR/workspaceS32DS.3.6.0/Dio_Example_S32K144/generate/include/Mcu_VS_0_PBcfg.h:
+../RTD/include/Mcu_EnvCfg.h:
+../RTD/include/Mcu_Ipw_Types.h:
+../RTD/include/Clock_Ip_Types.h:
+../RTD/include/Power_Ip_Types.h:
+../RTD/include/Ram_Ip_Types.h:
+../RTD/include/CDD_I2c.h:
+../RTD/include/I2c_Types.h:
+../RTD/include/I2c_Ipw_Types.h:
+../RTD/include/Lpi2c_Ip_Types.h:
+C\:/Users/EVR/workspaceS32DS.3.6.0/Dio_Example_S32K144/generate/include/Lpi2c_Ip_CfgDefines.h:
+C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K1_S32M24/RTD/BaseNXP_TS_T40D2M20I0R0/header/S32K144_LPI2C.h:
+../RTD/include/Lpi2c_Ip_Callbacks.h:
+../RTD/include/Lpi2c_Ip_Features.h:
+C\:/Users/EVR/workspaceS32DS.3.6.0/Dio_Example_S32K144/generate/include/CDD_I2c_CfgDefines.h:
+C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K1_S32M24/RTD/BaseNXP_TS_T40D2M20I0R0/include/I2c_MemMap.h:
+C\:/Users/EVR/workspaceS32DS.3.6.0/Dio_Example_S32K144/generate/include/CDD_I2c_Cfg.h:
+C\:/Users/EVR/workspaceS32DS.3.6.0/Dio_Example_S32K144/generate/include/CDD_I2c_VS_0_PBcfg.h:
+../RTD/include/I2c_Types.h:
+../RTD/include/Dem.h:
+C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K1_S32M24/RTD/BaseNXP_TS_T40D2M20I0R0/include/Std_Types.h:
+C\:/Users/EVR/workspaceS32DS.3.6.0/Dio_Example_S32K144/generate/include/Dem_IntErrId.h:
+../RTD/include/Dem_Types.h:
+../RTD/include/Rte_Dem_Type.h:
+../RTD/include/Dem_Types.h:
+../RTD/include/Rte_Dem_Type.h:
+C\:/NXP/S32DS.3.6.0/S32DS/software/PlatformSDK_S32K1_S32M24/RTD/BaseNXP_TS_T40D2M20I0R0/include/Dem_MemMap.h:
---
+++ Arduino/I2C_Oficial/Dio_Example_S32K144/Debug_FLASH/src/subdir.mk
@@ -0,0 +1,33 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/7-segment-display.c \
+../src/AS1115.c \
+../src/SevenSegments.c \
+../src/main.c 
+
+OBJS += \
+./src/7-segment-display.o \
+./src/AS1115.o \
+./src/SevenSegments.o \
+./src/main.o 
+
+C_DEPS += \
+./src/7-segment-display.d \
+./src/AS1115.d \
+./src/SevenSegments.d \
+./src/main.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Standard S32DS C Compiler'
+	arm-none-eabi-gcc "@src/7-segment-display.args" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
